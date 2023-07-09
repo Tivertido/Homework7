c35ce90fe63a0bb482f4ad1bdaa761a148363125
Tema <Tema>
2023-07-09 17:10:50 +0300

    commit_HW7_3

---
+++ HW7_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое* элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+// * Сумма всех цифр / их количество(это количество ваших строк)
+
+int[,] array = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        double[] average = new double[cols];
+
+        for (int j = 0; j < cols; j++)
+        {
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                sum += array[i, j];
+            }
+            average[j] = (double)sum / rows;
+        }
+
+        Console.WriteLine("Среднее арифметическое каждого столбца:");
+
+        for (int j = 0; j < cols; j++)
+        {
+            Console.WriteLine($"{average[j]}");
+        }
