e1ffaf1f0bc2f92ab93342703efb7bf414cd1747
Tema <Tema>
2023-07-09 17:00:35 +0300

    commit_HW7_1

---
+++ HW7_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными(тип double) числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк (m): ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        double[,] array = new double[m, n]; // Создаем двумерный массив
+
+        // Заполняем массив случайными числами
+        Random random = new Random();
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = random.NextDouble();
+            }
+        }
+
+        // Выводим массив на экран
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
